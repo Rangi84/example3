6ba07074374215eb8f1bea6406bc9e36899171c6
Your Name <you@example.com>
2020-04-21 00:51:37 +1000

    Converted the Rscript to Rmd

--- Practical 02- File Download.R
+++ Practical 02- File Download.R
@@ -1,4 +1,14 @@
-#  
+------
+Tittle: "example Rnd report"
+Author: "Rangi"
+Output: "html_document"
+theme: Cosmo
+-----
+  
+# Heading number 1 - download file and read into R.
+  
+Here we are using the 'download.file()' command to obtain the daaset, then we are readng it in to R. It is important to set 'header=TRUE' and 'row.names=1'. so that the data is formatted properly. 
+```{r.chunk1,echo = TRUE}
 # downloading the data.
 download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/mydata.tsv",
  destfile = "mydata.tsv")
@@ -8,6 +18,8 @@ x <- read.table("mydata.tsv") # to read the file downloaded.
 
 # Let's check if the data has imported properly.
 head(x) # to get the first 6 raws of the data set.
+
+# check data structure
 str(x)
 x <- read.table("mydata.tsv", header = TRUE)
 head(x) 
@@ -16,6 +28,10 @@ x <- read.table("mydata.tsv", header = TRUE , stringsAsFactors = FALSE )
 head(x)
 str(x)
 x <- read.table("mydata.tsv", header = TRUE , stringsAsFactors = FALSE , row.names = 1)
+```
+## Heading 2 - checking data intergrity
+Here we use the 'head()' command to look at the first 6 lines of the data frame, then 'str()' to make sure the contents are recgnised as numeric. 
+
 head(x)
 str(x)
 
